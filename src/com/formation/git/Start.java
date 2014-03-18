9e350c5477073b432cf2a373b13b42cf813956b3
Mohamed Lamhaouar <Mohamed.Lamhaouar@Nespresso.com>
2014-03-18 18:00:09 +0000

    mohamed lamhaouar first test to commit file by git

--- src/com/formation/git/Start.java
+++ src/com/formation/git/Start.java
@@ -4,7 +4,7 @@ package com.formation.git;
 public class Start {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("my first commit with git");
 		System.out.println("Git Training");
 		
 		System.out.println("The commit !!");
